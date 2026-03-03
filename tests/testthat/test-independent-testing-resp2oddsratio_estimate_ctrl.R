64d92530e9a00fb67a4207f92eac1591d8391c93
ShunRao8 <shunrao8@gmail.com>
2026-03-02 22:13:35 -0600

    Update test-independent-testing-resp2oddsratio_estimate_ctrl.R

--- tests/testthat/test-independent-testing-resp2oddsratio_estimate_ctrl.R
+++ tests/testthat/test-independent-testing-resp2oddsratio_estimate_ctrl.R
@@ -24,3 +24,61 @@ test_that("resp2oddsratio_estimate_ctrl() produces a smaller mean odds ratio whe
   expect_lt(exp(log_or_high$est), exp(log_or_low$est))
  
 })
+
+test_that("check the prior computation of mu and sigma on logit scale", {
+  low_soc_rr = 0.15
+  upp_soc_rr = 0.35
+  ci_rr      = 0.8
+
+  logit_low = log(low_soc_rr / (1 - low_soc_rr))
+  logit_upp = log(upp_soc_rr / (1 - upp_soc_rr))
+
+  mu_expected    = (logit_low + logit_upp) / 2
+  sigma_expected = (logit_upp - logit_low) / (2 * qnorm(1 - (1 - ci_rr) / 2))
+
+  # mu should be the midpoint of logit-transformed bounds
+  expect_equal(mu_expected, (logit_low + logit_upp) / 2)
+
+  # sigma should be positive
+  expect_gt(sigma_expected, 0)
+
+  # higher ci_rr => narrower normal quantile => larger sigma
+  sigma_narrow_ci = (logit_upp - logit_low) / (2 * qnorm(1 - (1 - 0.5) / 2))
+  sigma_wide_ci   = (logit_upp - logit_low) / (2 * qnorm(1 - (1 - 0.95) / 2))
+  expect_gt(sigma_narrow_ci, sigma_wide_ci)
+})
+
+test_that("check the output structure of resp2oddsratio_estimate_ctrl", {
+  n_resp_trt  = 33
+  n_trt       = 60
+  low_soc_rr  = 0.15
+  upp_soc_rr  = 0.35
+  ci_rr       = 0.8
+  niter       = 2000
+  nchains     = 1
+  seed        = 123
+
+  re = resp2oddsratio_estimate_ctrl(
+    n_resp_trt  = n_resp_trt,
+    n_trt       = n_trt,
+    low_soc_rr  = low_soc_rr,
+    upp_soc_rr  = upp_soc_rr,
+    ci_rr       = ci_rr,
+    niter       = niter,
+    nchains     = nchains,
+    seed        = seed
+  )
+
+  # check output contains est and se
+  expect_true(all(c("est", "se") %in% names(re)))
+  expect_equal(length(re), 2)
+
+  # check est and se are numeric scalars
+  expect_true(is.numeric(re$est))
+  expect_true(is.numeric(re$se))
+  expect_equal(length(re$est), 1)
+  expect_equal(length(re$se), 1)
+
+  # se must be positive
+  expect_gt(re$se, 0)
+})
